db8cd96a5538696b6d6c6a024c04726b2deb97a4
yinrong <yz576965161@126.com>
2016-11-09 18:32:40 +0800

    加入redis架包 加入事物配置

--- src/main/java/com/yinrong/service/impl/MemberServiceImpl.java
+++ src/main/java/com/yinrong/service/impl/MemberServiceImpl.java
@@ -4,16 +4,24 @@ import com.yinrong.dao.MemberDao;
 import com.yinrong.dataobject.MemberDo;
 import com.yinrong.service.MemberService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 
 /**
  * Created by yinrong on 2016/11/9.
  */
+//1 让checked例外也回滚：在整个方法前加上 @Transactional(rollbackFor=Exception.class)
+//
+//2 让unchecked例外不回滚： @Transactional(notRollbackFor=RunTimeException.class)
+//
+//3 不需要事务管理的(只查询的)方法：@Transactional(propagation=Propagation.NOT_SUPPORTED)
 @Service("memberService")
 public class MemberServiceImpl implements MemberService {
     @Resource
     MemberDao memberDao;
+    @Transactional
+
     public MemberDo queryLastCreate() {
         return memberDao.selectLastCreate();
     }
